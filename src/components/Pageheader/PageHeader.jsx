57840b2363f08176bd47f2ea36ee1c1ad22851a6
SyedMuhammadSabbor <sabborshah0501@gmail.com>
2024-08-08 18:33:36 +0500

    added  nav link

---
+++ src/components/Pageheader/PageHeader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Heading from "../heading/Heading";
+
+export default function PageHeader(props) {
+  return (
+    <>
+     <div className="surround">
+     <div className="link-name">
+        <Heading statement="About Us" option="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+          dignissimos quaerat exercitationem ducimus sapiente officia! Totam
+          accusam! Necessitatibus repellendus iure
+          reprehenderit temporibus maiores provident deleniti vel!
+        </p>
+      </div>
+     </div>
+    </>
+  );
+}
