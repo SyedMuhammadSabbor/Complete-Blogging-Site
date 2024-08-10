98baafb978a68008020627955641dbbc664e3db3
SyedMuhammadSabbor <sabborshah0501@gmail.com>
2024-08-10 23:17:12 +0500

    some improvements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,8 +35,6 @@ export default function Header() {
         setScrolled(true);
       }
     };
-   
-
     window.addEventListener("scroll", handleScroll);
     handleScroll(); 
     return () => {
