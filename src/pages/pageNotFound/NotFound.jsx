1e3c3ac5e0d42c4c92b61449c1bb44753197e37e
SyedMuhammadSabbor <sabborshah0501@gmail.com>
2024-08-12 22:05:30 +0500

    adding condition on wrong  commands

---
+++ src/pages/pageNotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+    
+      <h1>Page Not Found</h1>
+    
+    </div>
+  );
+};
+
+export default NotFound;
